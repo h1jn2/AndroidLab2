1556838ab695eaa8d5d587892d627b4781f4e0ae
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-05-14 15:29:16 +0900

    BroadCastReceiver Notification

---
+++ ch4/src/main/java/com/example/ch4/section2_notification/Test2_1Activity.kt
@@ -0,0 +1,85 @@
+package com.example.ch4.section2_notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch4.R
+import com.example.ch4.databinding.ActivityTest21Binding
+
+class Test2_1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityTest21Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 퍼미션 요청
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            if (it) {
+                noti()
+            } else {
+                Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(
+                    this,
+                    "android.permission.POST_NOTIFICATIONS"
+                ) == PackageManager.PERMISSION_GRANTED) {
+                    noti()
+                } else {
+                    permissionLauncher.launch("android.permission.POST_NOTIFICATIONS")
+                }
+            } else {
+                noti()
+            }
+        }
+    }
+
+    fun noti() {
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "oneChannel",
+                "My Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            // 만들어진 채널을 시스템에 등록
+            manager.createNotificationChannel(channel)
+            // 등록된 채널을 이용해 Builder 생성
+            builder = NotificationCompat.Builder(this, "oneChannel")
+        } else {
+            // Channel 개념이 없음
+            builder = NotificationCompat.Builder(this)
+        }
+
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setContentTitle("메시지 도착")
+        builder.setContentText("안녕하세요")
+
+        manager.notify(11, builder.build())
+    }
+}
